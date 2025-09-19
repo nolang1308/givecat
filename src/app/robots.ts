a00be67247c3f3009dafae377af46137047ce6fd
DOTORY <suminhwang1308@gmail.com>
2025-09-19 12:10:46 +0900

    Add static sitemap.xml file to fix Google Search Console indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://givecat.vercel.app/sitemap.xml',
+    sitemap: 'https://giftcat.vercel.app/sitemap.xml',
   }
 }
